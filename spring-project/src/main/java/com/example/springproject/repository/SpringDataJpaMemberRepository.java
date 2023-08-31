885f303b9cd62b54e697cc336b30234c4cbf64b2
Jiyoon Kim <rlawldbs0911@naver.com>
2023-09-01 05:11:23 +0900

    Section 6. 스프링 DB 접근 기술 - 스프링 데이터 JPA

---
+++ spring-project/src/main/java/com/example/springproject/repository/SpringDataJpaMemberRepository.java
@@ -0,0 +1,9 @@
+package com.example.springproject.repository;
+
+import com.example.springproject.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
+    Optional<Member> findByName(String name);
+}
